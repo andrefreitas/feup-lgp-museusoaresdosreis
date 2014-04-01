28cb2de1e8beae41bb3b47a8f9ab85e395983974
Fernando Vieira <frpv74@gmail.com>
2014-04-01 04:35:25 -0700

    init conf

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Museusoaresreis::Application.config.secret_key_base = '2367c7eeed53c030a3a026f6bceb0d89b38894edd5acdb0e9fbe3b482dbef46ad7a23a8c6283b9c424bbe7e9081e120d834b971412ad6d19c6a2574126d16ac4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Museusoaresreis::Application.config.session_store :cookie_store, key: '_museusoaresreis_session'
