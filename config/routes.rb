5f5cc2547b47ec13c03ebb0b2edbe1be678f0d47
André Freitas <p.andrefreitas@gmail.com>
2014-04-11 10:38:29 +0100

    Criada a view do login da administração

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Museusoaresreis::Application.routes.draw do
+  get "admin/login"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
