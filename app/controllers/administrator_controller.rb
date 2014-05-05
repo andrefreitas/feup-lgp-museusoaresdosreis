a9ea92b6692ee8a138520b92afb3489d2b19f8e0
André Freitas <p.andrefreitas@gmail.com>
2014-05-05 19:40:27 +0100

    Commit ridículo mas necessário

--- app/controllers/administrator_controller.rb
+++ app/controllers/administrator_controller.rb
@@ -4,6 +4,7 @@ class AdministratorController < ApplicationController
 
   def new
   end
+    
   def create
    @administrator = Administrator.create(:name => params['name'], :email => params['email'], :password => params['password'])
    respond_to do |format|
