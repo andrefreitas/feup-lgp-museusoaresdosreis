0cfe64a92a36b403858fbd7768588c6b42b712d6
André Freitas <p.andrefreitas@gmail.com>
2014-05-22 14:38:48 +0100

    Fix da interface de edit de evento e das acções da timeline.

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -4,7 +4,7 @@ class MediaController < ApplicationController
   end
 
   def getDates
-    @dates = Event.all.select("date").map(&:date)
+    @dates = Event.all.select("date").map(&:date).uniq
     respond_to do |format|
       format.json{render :json => @dates}
     end
